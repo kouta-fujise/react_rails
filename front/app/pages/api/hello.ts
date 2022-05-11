01520bdf703878300e68ac21ec63ed16f512b429
藤瀬功大 <sketchbooks2490@gmail.com>
2022-05-11 17:20:10 +0900

    add docker-compose(#1)

---
+++ front/app/pages/api/hello.ts
@@ -0,0 +1,16 @@
+import React from 'react'
+import {AppProps} from 'next/app';
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
+
+
+// typescript導入前のコード
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
